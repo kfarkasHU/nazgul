832e3b916b4500e8a39aad273a3fcfb80cb150d3
kfarkasHU <kolos.farkas@dakai.io>
2024-04-26 15:39:14 +0200

    refactor(http): remove unused ctx utilities

--- packages/nazgul.server/src/http/context.ts
+++ packages/nazgul.server/src/http/context.ts
@@ -3,7 +3,6 @@ import { Application, Router } from "express";
 export class HttpContext {
 
     private static _server: Application | undefined;
-    private static _router = Router();
 
     public static setServer(server: Application): void {
         this._server = server;
@@ -14,21 +13,6 @@ export class HttpContext {
             throw new Error("HttpContext::_server is nil!");
         }
         return this._server;
-    }
-
-    public static addGetRoute(
-        path: string,
-        handler: () => void
-    ): void {
-        this._router.get(path, () => handler())
-    }
-
-    public static getRouteContext(): Router {
-        return this._router;
-    }
-
-    public static resetRouteContext(): void {
-        this._router = Router();
     }
 
 }
