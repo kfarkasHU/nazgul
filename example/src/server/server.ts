849c378659a491b2306bc121acb023718f969842
kfarkasHU <kolos.farkas@dakai.io>
2024-04-26 16:05:14 +0200

    feat(server): example for `listAllRoutes()`

--- example/src/server/server.ts
+++ example/src/server/server.ts
@@ -5,4 +5,13 @@ const server = express()
 HttpContext.setServer(server);
 import "../controller";
 
-server.listen(9000);
+server.listen(9000, () => {
+    const routes = HttpContext
+        .listAllRoutes()
+        .map(m => {
+            const method = `[${m.method}]`.padEnd(15, " ");
+            return `${method}${m.fullPath}`;
+        });
+    ;
+    routes.forEach(m => console.log(m));
+});
