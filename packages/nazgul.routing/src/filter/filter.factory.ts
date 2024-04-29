777bf298751d0784e8625af86345101372a712cc
kfarkasHU <kolos.farkas@dakai.io>
2024-04-29 22:09:02 +0200

    feat(filter): add factory

---
+++ packages/nazgul.routing/src/filter/filter.factory.ts
@@ -0,0 +1,23 @@
+import { BaseHttpController, HttpHandler, INazgulFilterAttribute } from "@kfarkashu/nazgul.core";
+import { Request, Response, NextFunction } from "express";
+
+export class FilterAttributeFactory {
+    public static create(
+        handler: (req: Request, res: Response, next: NextFunction) => Promise<boolean> | boolean
+    ): INazgulFilterAttribute {
+        return <T extends BaseHttpController>(
+            target: T,
+            property: keyof T
+        ) => {
+            if (!target.__handleCandidates) {
+                target.__handleCandidates = {}
+            }
+            const handle = (target.__handleCandidates[property.toString()] || {}) as HttpHandler<T>;
+
+            target.__handleCandidates[property.toString()] = {
+                ...handle,
+                filters: (handle.filters || []).concat(handler)
+            }
+        }
+    }
+}
