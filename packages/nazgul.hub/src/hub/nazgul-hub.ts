286739d5e5a31f99014bbb7695c0aafa8f9da19c
kfarkasHU <kolos.farkas@dakai.io>
2024-04-29 22:08:46 +0200

    feat(hub): add server configuration to hub

--- packages/nazgul.hub/src/hub/nazgul-hub.ts
+++ packages/nazgul.hub/src/hub/nazgul-hub.ts
@@ -1,8 +1,17 @@
 import { INazgulPlugin } from "@kfarkashu/nazgul.core";
+import { Application } from "express";
 
 import { PluginHub } from "../plugin";
+import { HttpContext } from "../http";
 
 export class NazgulHub {
+
+    public static addServer(
+        server: Application
+    ): typeof NazgulHub {
+        HttpContext.setServer(server);
+        return this;
+    }
 
     public static usePlugin(
         plugin: INazgulPlugin
