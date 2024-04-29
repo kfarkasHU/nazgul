24e3f9f8b829bb4af4206bfaf9155ffda2b14aaf
kfarkasHU <kolos.farkas@dakai.io>
2024-04-30 01:55:58 +0200

    chore(*): general cleanup

--- example/src/server/server.ts
+++ example/src/server/server.ts
@@ -1,18 +1,13 @@
 import express from "express";
 import { HttpContext, NazgulHub } from "nazgul.hub";
-import { NazgulApidocPlugin } from "nazgul.apidoc";
-import { NazgulRoutingPlugin } from "nazgul.routing";
 
 const server = express()
 
 NazgulHub
     .addServer(server)
-    .usePlugin(new NazgulApidocPlugin())
-    .usePlugin(new NazgulRoutingPlugin())
 ;
 
 import "../controller";
-import {  } from "nazgul.hub";
 
 server.listen(9000, () => {
     const routes = HttpContext
